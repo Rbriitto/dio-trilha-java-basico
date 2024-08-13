4ff7e1e88618195cf7330e41cc434410353398bb
Rbriitt <rodrigobritto.web@gmail.com>
2024-08-12 21:06:58 -0300

    segunda atualizacao

--- tipos-variaveis/src/TiposVariaveis.java
+++ tipos-variaveis/src/TiposVariaveis.java
@@ -1,5 +1,24 @@
 public class TiposVariaveis {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        short numeroCurto = 1;
+        int numeroNormal = numeroCurto;
+        short numeroCurto2 = (short) numeroNormal; 
+    
+   
+    int numero = 5;
+
+    numero = 10 ;
+
+    final double VALOR_DE_PI = 3.14;
+
+    //FINAL SÃO CONSTANTES NÃO PODE SER ALTERADAS E DEVE SER ESCRITA EM CAIXA ALTA
+
+    //ESTUDAR TODOS OS TIPOS PRIMITIVOS 
+    //ESTUDAR A CLASSE STRING QUE REPRESENTA TEXTOS NA APPLICAÇÃO
+    String meuNome = "Rodrigo Britto";
+    
+    
     }
+
 }
+//cast 
